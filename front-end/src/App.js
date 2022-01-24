e330d1439453983ae9d3312dc41c915ca50f3f67
MarcoDev <mrichet.cb@gmail.com>
2022-01-24 17:07:30 +0100

    Setting react-router-dom V6

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,25 @@
 import React from "react";
-import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
-
-
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <>
-    <Header />
-    <main className="py-3">
-      <Container>
-      <HomeScreen />
-      </Container>
-    </main>
-    <Footer />
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ front-end/src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductScreen = () => {
+  return (
+  <div>
+      Productscreen
+  </div>
+  )
+};
+
+export default ProductScreen;
