4d41067e36d37caf772ff78b04d10831fcd88027
MarcoDev <mrichet.cb@gmail.com>
2022-01-27 17:34:56 +0100

    Fetching products and single product in frontend using useEffect

--- front-end/src/screens/ProductScreen.js
+++ front-end/src/screens/ProductScreen.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
+import axios from "axios";
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState({});
+
   const params = useParams();
-  const product = products.find((p) => p._id === params.id);
+
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const {data} = await axios.get(`/api/products/${params.id}`);
+      setProduct(data);
+    }
+    fetchProduct();
+  }, []);
+
+
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
